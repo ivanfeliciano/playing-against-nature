b3f07f6ce11393f1ee9121a523f367b01cbbfd2e
Ivan <ivan.felavel@gmail.com>
2020-04-29 00:51:26 -0500

    Resultados experimentos

--- guided_q_learning/scripts/run_experiments.sh
+++ guided_q_learning/scripts/run_experiments.sh
@@ -1,12 +1,36 @@
 EPISODES=1000
-ST=one_to_one
 MOD=50
 
-python run_light_env_experiments.py --num 5 --structure $ST --episodes $EPISODES --mod $MOD
-python run_light_env_experiments.py --num 7 --structure $ST --episodes $EPISODES --mod $MOD
-python run_light_env_experiments.py --num 9 --structure $ST --episodes $EPISODES --mod $MOD
+ST=one_to_one
+
+time python run_light_env_experiments.py --num 5 --structure $ST --episodes $EPISODES --mod $MOD
+time python run_light_env_experiments.py --num 7 --structure $ST --episodes $EPISODES --mod $MOD
+time python run_light_env_experiments.py --num 9 --structure $ST --episodes $EPISODES --mod $MOD
 
 
-python run_light_env_experiments.py --num 5 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
-python run_light_env_experiments.py --num 7 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
-python run_light_env_experiments.py --num 9 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
+time python run_light_env_experiments.py --num 5 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
+time python run_light_env_experiments.py --num 7 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
+time python run_light_env_experiments.py --num 9 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
+
+
+ST=many_to_one
+
+time python run_light_env_experiments.py --num 5 --structure $ST --episodes $EPISODES --mod $MOD
+time python run_light_env_experiments.py --num 7 --structure $ST --episodes $EPISODES --mod $MOD
+time python run_light_env_experiments.py --num 9 --structure $ST --episodes $EPISODES --mod $MOD
+
+
+time python run_light_env_experiments.py --num 5 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
+time python run_light_env_experiments.py --num 7 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
+time python run_light_env_experiments.py --num 9 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
+
+ST=one_to_many
+
+time python run_light_env_experiments.py --num 5 --structure $ST --episodes $EPISODES --mod $MOD
+time python run_light_env_experiments.py --num 7 --structure $ST --episodes $EPISODES --mod $MOD
+time python run_light_env_experiments.py --num 9 --structure $ST --episodes $EPISODES --mod $MOD
+
+
+time python run_light_env_experiments.py --num 5 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
+time python run_light_env_experiments.py --num 7 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
+time python run_light_env_experiments.py --num 9 --structure $ST --episodes $EPISODES --mod $MOD --stochastic
